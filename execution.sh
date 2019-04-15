b9a17fa288616fe085d62180133daf6c215fbe17
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-04-15 20:54:29 -0300

    Pegando ids com gettid além do pthread_self.

--- execution.sh
+++
@@ -1,6 +0,0 @@
-# gcc main.c -o program -lpthread
-gcc main2.c -o program -lpthread
-
-chmod +x program
-
-./program
--- main.c
+++ main.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+// #define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 
-// Função que incrementa x até vlor de x ser maior ou igual a 100
-void *inc_x(void *x_void_ptr)
+
+
+void *print_thread(void *void_pointer) {
+    //int_ptr receberá o valor o ponteiro para o valor de i
+
+    pid_t tid;
+    tid = syscall(SYS_gettid);
+
+    int *int_ptr = (int *) void_pointer;
+    printf("Eu sou a Thread%d e meu ID é pthread_self: %u ou com gettid: %u.\n", *int_ptr, (uint) pthread_self(), tid);
+    pthread_exit(NULL);
+    return NULL;
+}
+
+int main()
 {
-	/* incremente x até 100 */
-	int *x_ptr = (int *)x_void_ptr;
-	while(++(*x_ptr) < 100){
-		printf("\nx = %d\n", *x_ptr);
-	};
+    int n;
+    printf("\n Digite o número de thread que deseja criar: ");
+    scanf("%d", &n);
 
-	printf("x increment finished successfuly!\n");
+    printf("\n Você escolheu criar %d threads. \n", n);
 
-	/* the function must return something - NULL will do */
-	return NULL;
+    pthread_t array_of_threads[n];
+
+    int i = 0;
+    while (++i <= n) {
+        pthread_create(&array_of_threads[i], NULL, print_thread, &i);
+    }
+
+    i = 0;
+    while (++i <= n) {
+        pthread_join(array_of_threads[i], NULL);
+    }
+    return 0;
 }
 
+// Output
+//  Digite o número de thread que deseja criar: 3
 
-int main() {
-	int x = 0, y = 0;
-
-	printf("\nx = %d, y = %d\n", x, y);
-
-	pthread_t inc_x_thread;
-
-	// pthread_crete() retorna 0 se sucesso
-	if(pthread_create(&inc_x_thread, NULL, inc_x, &x)) {
-		
-		fprintf(stderr, "Error creating thread.\n");
-		return 1;
-	}
-
-	while(++y < 100){
-		printf("\ny = %d\n", y);
-	};
-
-	printf("y increment finished.\n");
-	
-	if(pthread_join(inc_x_thread, NULL)) {
-		fprintf(stderr, "Error joining thread.\n");
-		return 2;
-	}
-
-	printf("x = %d, y = %d\n", x, y);
-
-	return 0;
-
-}
+//  Você escolheu criar 3 threads. 
+//     Eu sou a Thread1 e meu ID é 3044779776.
+//     Eu sou a Thread1 e meu ID é 3053172480.
+//     Eu sou a Thread1 e meu ID é 3061565184.
