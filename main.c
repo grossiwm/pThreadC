7ca0e7cf8f3fed2478055c5cb78fc8f7b1dc3a60
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-05-30 15:42:09 -0300

    Criado array de index de threads para passar por referência ao criar thread.

--- main.c
+++ main.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-// #define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 
-void *print_thread(void *void_pointer) {
-    //int_ptr receberá o valor o ponteiro para o valor de i
+void generateThreadsIndexArray(int *array, int size);
+void *print_thread(void *void_pointer);
 
-    pid_t tid;
-    tid = syscall(SYS_gettid);
-
-    int *int_ptr = (int *) void_pointer;
-    printf("Eu sou a Thread%d e meu ID é pthread_self: %u ou com gettid: %u.\n", *int_ptr, (uint) pthread_self(), tid);
-    pthread_exit(NULL);
-    return NULL;
-}
 
 int main()
 {
@@ -27,23 +18,39 @@ int main()
     printf("\n Você escolheu criar %d threads. \n", n);
 
     pthread_t array_of_threads[n];
+    // int id[n]
+
+    int threadsIndexesArray[n];
+    generateThreadsIndexArray(threadsIndexesArray, n);
 
     int i = 0;
-    while (++i <= n) {
-        pthread_create(&array_of_threads[i], NULL, print_thread, &i);
+    while (i < n) {
+        pthread_create(&array_of_threads[i], NULL, print_thread, (void *) &threadsIndexesArray[i]);
+        i++;
     }
 
     i = 0;
-    while (++i <= n) {
+    while (i < n) {
         pthread_join(array_of_threads[i], NULL);
+        i++;
     }
     return 0;
 }
 
-// Output
-//  Digite o número de thread que deseja criar: 3
+void *print_thread(void *void_pointer) {
+    //int_ptr receberá o valor o ponteiro para o valor de i
 
-//  Você escolheu criar 3 threads. 
-//     Eu sou a Thread1 e meu ID é 3044779776.
-//     Eu sou a Thread1 e meu ID é 3053172480.
-//     Eu sou a Thread1 e meu ID é 3061565184.
+    pid_t tid;
+    tid = syscall(SYS_gettid);
+
+    int *int_ptr = (int *) void_pointer;
+    printf("Eu sou a Thread0%d e meu ID é pthread_self: %u ou com gettid: %u.\n", *int_ptr, (uint) pthread_self(), tid);
+
+    return NULL;
+}
+
+void generateThreadsIndexArray(int *array, int size) {
+    for (int i = 0; i < size; i++) {
+      array[i] = i + 1;
+    }
+}
